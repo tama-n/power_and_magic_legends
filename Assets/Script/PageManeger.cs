5ff630af4adf0b79d43f3cac6a2a775721c8c092
mognin <yiteng990@gmail.com>
2026-07-18 11:39:45 +0900

    チュートリアル立体視

--- Assets/Script/PageManeger.cs
+++ Assets/Script/PageManeger.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class TutorialPageManager : MonoBehaviour
 {
-    [SerializeField] private GameObject introPage;
-    [SerializeField] private GameObject movePage;
-    [SerializeField] private GameObject attackPage;
-    [SerializeField] private GameObject upgradePage;
-    [SerializeField] private GameObject transGamePage;
+    [Header("--- L側（左目用）のページ ---")]
+    [SerializeField] private GameObject introPageL;
+    [SerializeField] private GameObject movePageL;
+    [SerializeField] private GameObject attackPageL;
+    [SerializeField] private GameObject upgradePageL;
+    [SerializeField] private GameObject transGamePageL;
+
+    [Header("--- R側（右目用）のページ ---")]
+    [SerializeField] private GameObject introPageR;
+    [SerializeField] private GameObject movePageR;
+    [SerializeField] private GameObject attackPageR;
+    [SerializeField] private GameObject upgradePageR;
+    [SerializeField] private GameObject transGamePageR;
 
     private void Start()
     {
@@ -16,39 +24,50 @@ public class TutorialPageManager : MonoBehaviour
     public void ShowIntroPage()
     {
         HideAllPages();
-        introPage.SetActive(true);
+        if (introPageL != null) introPageL.SetActive(true);
+        if (introPageR != null) introPageR.SetActive(true);
     }
 
     public void ShowMovePage()
     {
         HideAllPages();
-        movePage.SetActive(true);
+        if (movePageL != null) movePageL.SetActive(true);
+        if (movePageR != null) movePageR.SetActive(true);
     }
 
     public void ShowAttackPage()
     {
         HideAllPages();
-        attackPage.SetActive(true);
+        if (attackPageL != null) attackPageL.SetActive(true);
+        if (attackPageR != null) attackPageR.SetActive(true);
     }
 
     public void ShowUpgradePage()
     {
         HideAllPages();
-        upgradePage.SetActive(true);
+        if (upgradePageL != null) upgradePageL.SetActive(true);
+        if (upgradePageR != null) upgradePageR.SetActive(true);
     }
 
     public void ShowTransGamePage()
     {
         HideAllPages();
-        transGamePage.SetActive(true);
+        if (transGamePageL != null) transGamePageL.SetActive(true);
+        if (transGamePageR != null) transGamePageR.SetActive(true);
     }
 
     private void HideAllPages()
     {
-        introPage.SetActive(false);
-        movePage.SetActive(false);
-        attackPage.SetActive(false);
-        upgradePage.SetActive(false);
-        transGamePage.SetActive(false);
+        if (introPageL != null) introPageL.SetActive(false);
+        if (movePageL != null) movePageL.SetActive(false);
+        if (attackPageL != null) attackPageL.SetActive(false);
+        if (upgradePageL != null) upgradePageL.SetActive(false);
+        if (transGamePageL != null) transGamePageL.SetActive(false);
+
+        if (introPageR != null) introPageR.SetActive(false);
+        if (movePageR != null) movePageR.SetActive(false);
+        if (attackPageR != null) attackPageR.SetActive(false);
+        if (upgradePageR != null) upgradePageR.SetActive(false);
+        if (transGamePageR != null) transGamePageR.SetActive(false);
     }
 }
